a3cd66d5b70c40aed5a6ecb4696e5e5fdebf45bb
Alex Levine <a17levine@gmail.com>
2023-04-21 02:47:27 -0500

    Add errors

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -2,7 +2,12 @@ class ForecastsController < ApplicationController
   def show
     if params[:q].present?
       @forecast = Forecast.new(address_input: params[:q])
-      @forecast.fetch
+      begin
+        @forecast.fetch
+      rescue
+        @forecast = nil
+        flash[:alert] = "Invalid input, please check the address."
+      end
     end
   end
 end
